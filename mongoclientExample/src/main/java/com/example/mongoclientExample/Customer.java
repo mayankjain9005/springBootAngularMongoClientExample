7d76fd667dc7e6f4de1c69c21d5f372ddd976153
mayankjain9005 <mayankjain9005@gmail.com>
2020-05-17 03:10:48 +0530

    first commit

---
+++ mongoclientExample/src/main/java/com/example/mongoclientExample/Customer.java
@@ -0,0 +1,15 @@
+package com.example.mongoclientExample;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customer {
+	private String name;
+	private int phone;
+	private String address;
+	
+	
+
+}
---
+++ mongoclientExample/src/test/java/com/example/mongoclientExample/MongoclientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongoclientExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoclientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
